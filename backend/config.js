3b30f7144b01df88e6cd048cc7d225b246f01e5f
Harshal Ghuge <hxrsh369@gmail.com>
2026-06-08 13:08:25 +0530

    README
    Changed mongoDBURL to an empty string for security.

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 export const PORT = 5555;
 
-export const mongoDBURL = "mongodb+srv://hxrsh369_db_user:Bhau8975@cluster0.qorrsoi.mongodb.net/?appName=Cluster0";
+export const mongoDBURL = "";
 
 
 // Please create a free database for yourself.
